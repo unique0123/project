8ee6cf96b34f83be9a47459ea7741d68c47a5384
unique0123 <127250736+unique0123@users.noreply.github.com>
2023-11-12 03:44:11 +0100

    Add files via upload

---
+++ anew start/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.querySelector(".sidenav").style.width = "250px";
+  document.querySelector(".main-content").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.querySelector(".sidenav").style.width = "0";
+  document.querySelector(".main-content").style.marginLeft = "0";
+}
